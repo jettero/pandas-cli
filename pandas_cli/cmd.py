5461e765ea11c87b7f2ed33ae2a218e7449a6ee4
Paul Miller <paul@jettero.pl>
2022-09-22 10:21:42 -0400

    clicked

--- pandas_cli/cmd.py
+++ pandas_cli/cmd.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import click
+
+from .version import __version__ as VERSION
+
+@click.command()
+@click.version_option(version=VERSION)
 def main():
     pass
