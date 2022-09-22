838bcff51d0bcdce7636499ee89f42ca58ace04b
Paul Miller <paul@jettero.pl>
2022-09-22 10:29:04 -0400

    I want to like click, but it's just so much harder than argparser

--- pandas_cli/cmd.py
+++ pandas_cli/cmd.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import click
-
+import sys
+import argparse
+import pandas as pd
 from .version import __version__ as VERSION
 
-@click.command()
-@click.version_option(version=VERSION)
 def main():
-    pass
+    parser = argparse.ArgumentParser(  # description='this program',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    parser.add_argument("input", nargs="*", default=[sys.stdin], type=argparse.FileType('r', encoding='utf-8'))
+    parser.add_argument("-v", "--verbose", action="count", default=0, help='increase verbosity for each -v')
+    parser.add_argument("--version", action="version", version=VERSION)
+
+    args = parser.parse_args()
+
+
+    print(args)
