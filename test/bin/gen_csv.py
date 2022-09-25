1f22de3fb16ea63e56f09dbd2796844e621e4f56
Paul Miller <paul@jettero.pl>
2022-09-25 13:15:29 -0400

    psf-black

--- test/bin/gen_csv.py
+++ test/bin/gen_csv.py
@@ -4,12 +4,13 @@ import os, sys
 import argparse
 import random
 
+
 def main(*margs):
     parser = argparse.ArgumentParser()
     parser.add_argument("-r", "--rows", type=int, default=int(os.environ.get("LINES", 25)) - 5)
     parser.add_argument("-c", "--column", action="append", type=str)
     parser.add_argument("-p", "--precision", type=int, default=4)
-    parser.add_argument('-o', '--output-file', type=argparse.FileType('w'), default=sys.stdout);
+    parser.add_argument("-o", "--output-file", type=argparse.FileType("w"), default=sys.stdout)
 
     if margs:
         args = parser.parse_args(margs)
@@ -23,5 +24,6 @@ def main(*margs):
         l = [f"{(j+1) + 0.5 + 0.5*random.random():0.{args.precision}f}" for j, v in enumerate(headers)]
         print(",".join(l), file=args.output_file)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- test/test_generators.py
+++ test/test_generators.py
@@ -3,8 +3,10 @@
 
 import os
 
+
 def test_csv_gen(csv1):
     assert os.path.isfile(csv1)
+
 
 def test_json_gen(json1):
     assert os.path.isfile(json1)
