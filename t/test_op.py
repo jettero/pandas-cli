2aba42f5183fcbe4fd5d80a25d74faae9c59a8c2
Paul Miller <paul@jettero.pl>
2024-09-17 16:14:13 -0400

    100% coverage

--- t/test_op.py
+++ t/test_op.py
@@ -4,13 +4,16 @@
 import pytest
 import pdc.op
 
+
 def df_zipper(*df):
     yield from zip(*(sorted(x.to_records(index=False).tolist()) for x in df))
+
 
 def test_concat_without_key(ta_test1, ta_test2, ta_t1Ct2):
     df = pdc.op.concat(ta_test1.df, ta_test2.df)
     for lhs, rhs in df_zipper(df, ta_t1Ct2.df):
         assert lhs == rhs
+
 
 def test_concat_with_key(ta_test1, ta_test2, ta_t1Ct2Kvar):
     for k in ("var", ("var",), ["var"]):
@@ -21,11 +24,16 @@ def test_concat_with_key(ta_test1, ta_test2, ta_t1Ct2Kvar):
     with pytest.raises(ValueError):
         pdc.op.concat(ta_test1.df, ta_test2.df, key=(x for x in range(10)))
 
+
 def test_transpocat_with_args(ta_test1, ta_test2, ta_t1TCt2Kvar):
     for k in ("var", ("var",), ["var"]):
         df = pdc.op.transpocat(ta_test1.df, ta_test2.df, key=k, merge_type="outer")
         for lhs, rhs in df_zipper(df, ta_t1TCt2Kvar.df):
             assert lhs == rhs
+
+    with pytest.raises(ValueError):
+        pdc.op.transpocat(ta_test1.df, ta_test2.df, key=(x for x in range(10)))
+
 
 def test_transpocat_without_args(ta_test1, ta_test2, ta_t1TCt2):
     df = pdc.op.transpocat(ta_test1.df, ta_test2.df)
@@ -33,9 +41,19 @@ def test_transpocat_without_args(ta_test1, ta_test2, ta_t1TCt2):
         assert lhs == rhs
 
 
-def test_filter(ta_test1, ta_test2):
-    df = pdc.op.filter(ta_test1.df, ta_test2.df, key="var")
+def test_filter_with_key(ta_test1, ta_test2):
+    for k in ("var", ("var",), ["var"]):
+        df = pdc.op.filter(ta_test1.df, ta_test2.df, key=k)
+        ldf = len(df)
+        lta = len(ta_test1.df)
+        assert ldf == lta - 1
+
+    with pytest.raises(ValueError):
+        pdc.op.filter(ta_test1.df, ta_test2.df, key=(x for x in range(10)))
+
+
+def test_filter_without_key(ta_test1, ta_test2):
+    df = pdc.op.filter(ta_test1.df, ta_test2.df)
     ldf = len(df)
     lta = len(ta_test1.df)
-
-    assert ldf == lta - 1
+    assert ldf == lta
