9faa74ae0e7af8b4f0a46a2b3c70ec51021aad66
Paul Miller <paul@jettero.pl>
2024-09-18 11:40:58 -0400

    update comment for clarity / run psf-black

--- pdc/op.py
+++ pdc/op.py
@@ -21,12 +21,12 @@ def concat(A, B, key=None):
         return df
     elif isinstance(key, list):
         pass
-    elif isinstance(key, (tuple,set)):
+    elif isinstance(key, (tuple, set)):
         key = list(key)
     elif isinstance(key, str):
         key = [key]
     else:
-        raise ValueError(f'key={key} is invalid. should be a tuple of strings or something')
+        raise ValueError(f"key={key} is invalid. should be a tuple of strings or something")
     return df.drop_duplicates(subset=key, keep="last")
 
 
@@ -38,12 +38,12 @@ def transpocat(A, B, key=None, merge_type="outer"):
         key = A.columns.tolist()
     elif isinstance(key, list):
         pass
-    elif isinstance(key, (tuple,set)):
+    elif isinstance(key, (tuple, set)):
         key = list(key)
     elif isinstance(key, str):
         key = [key]
     else:
-        raise ValueError(f'key={key} is invalid. should be a tuple of strings or something')
+        raise ValueError(f"key={key} is invalid. should be a tuple of strings or something")
     A = pd.merge(A, B, on=key, suffixes=("", DING_DING_DING), how=merge_type)
     c = A.columns.tolist()
     d = [x for x in c if f"{x}{DING_DING_DING}" in c]
@@ -51,20 +51,21 @@ def transpocat(A, B, key=None, merge_type="outer"):
         A[lhs] = A[lhs].combine_first(A[f"{lhs}{DING_DING_DING}"])
     return A.drop(columns=[f"{x}{DING_DING_DING}" for x in d])
 
+
 def filter(A, B, key=None):
-    #  axy   def   axy
-    #  bwj - ghi = ckl
-    #  ckl   baz
+    #  axx   ayy   bxx
+    #  bxx - dyy = cxx
+    #  cxx   eyy
     if key is None:
         key = A.columns.tolist()
     elif isinstance(key, list):
         pass
-    elif isinstance(key, (tuple,set)):
+    elif isinstance(key, (tuple, set)):
         key = list(key)
     elif isinstance(key, str):
         key = [key]
     else:
-        raise ValueError(f'key={key} is invalid. should be a tuple of strings or something')
+        raise ValueError(f"key={key} is invalid. should be a tuple of strings or something")
     A["unique_key"] = A[key].apply(tuple, axis=1)
     B["unique_key"] = B[key].apply(tuple, axis=1)
     return A[~A.unique_key.isin(B.unique_key)].drop(columns="unique_key")
