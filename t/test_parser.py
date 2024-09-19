58bc7a56e22589bb50080f4e9f44ac2d4d59212a
Paul Miller <paul@jettero.pl>
2024-09-19 15:34:56 -0400

    unstash parser concat tests

--- t/test_parser.py
+++ t/test_parser.py
@@ -30,9 +30,15 @@ def test_idx(listoid):
     assert j() == None
 
 
+def test_invalid_file_fail():
+    with pytest.raises(ValueError):
+        pdc.parser.parse("f1 + f2", files=(3, 4, 5))
+
+
 def test_op_idx_missed_items(listoid):
-    # This is missed in the actual parser test because src= is always specified
-    # but I wanted to leave the None=>tmpvz implication if block.
+    # The parser always specifies src=<something>.
+    # but it's worth testing what happens if you
+    # omit the kwarg
     i = transformer.op_idx("0")
     assert i.src is transformer.tmpvz
 
@@ -74,27 +80,33 @@ def test_concat_kwargs(ta_test1, ta_test2, ta_t1Ct2Kvar):
         assert lhs == rhs
 
 
-# def test_concat_nested_kwargs(ta_test1, ta_test2, ta_test3, ta_t1Ct2KvarCt3):
-#     pf = pdc.parser.parse("f1 + f2 @ var; t1 + f3", files=(ta_test1, ta_test2, ta_test3))  # parsed frame
+def test_concat_nested_kwargs(ta_test1, ta_test2, ta_test3, ta_t1Ct2Kvar, ta_t1Ct2KvarCt3):
+    pf = pdc.parser.parse("f1 + f2 @ var; t1 + f3", files=(ta_test1, ta_test2, ta_test3))
 
-#     assert pf.args[1].deref is ta_test3
-#     assert pf.kw == dict()
+    assert pf.args[1].deref is ta_test3
+    assert pf.kw == dict()
 
-#     ipf = pf.args[0].deref  # inner parsed frame
-#     assert ipf.args[0].deref is ta_test1
-#     assert ipf.args[1].deref is ta_test2
-#     assert ipf.kw == dict(key=set(["var"]))
+    ipf = pf.args[0].deref  # inner parsed frame
+    assert ipf.args[0].deref is ta_test1
+    assert ipf.args[1].deref is ta_test2
+    assert ipf.kw == dict(key=set(["var"]))
 
-#     df = pf()
+    ##########################
+    t1 = pf.args[0]()
 
-#     ldf = len(df)
-#     lta = len(ta_t1Ct2KvarCt3)
-#     assert ldf == lta
+    lt1 = len(t1)
+    lta = len(ta_t1Ct2Kvar)
+    assert lt1 == lta
 
-#     for lhs, rhs in df_zipper(df, ta_t1Ct2KvarCt3.df):
-#         assert lhs == rhs
+    for lhs, rhs in df_zipper(t1, ta_t1Ct2Kvar.df):
+        assert lhs == rhs
 
+    ##########################
+    df = pf()
 
-# def test_invalid_file_fail():
-#     with pytest.raises(ValueError):
-#         pdc.parser.parse("f1 + f2", files=(3, 4, 5))
+    ldf = len(df)
+    lta = len(ta_t1Ct2KvarCt3)
+    assert ldf == lta
+
+    for lhs, rhs in df_zipper(df, ta_t1Ct2KvarCt3.df):
+        assert lhs == rhs
